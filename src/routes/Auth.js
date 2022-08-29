7508802e5abda1722b7cbdb4d5a07b113607adac
semetea <55260856+semetea@users.noreply.github.com>
2022-08-29 14:57:37 -0400

    log in

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -9,6 +9,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("")
     const auth = getAuth();
     const onChange = (event) => {
         const { target: { name, value } } = event;
@@ -29,16 +30,22 @@ const Auth = () => {
             }
             console.log(data)
         } catch (error) {
-            console.log(error)
+            setError(error.message)
         }
+    }
+
+    const toggleAccount = () => {
+        setNewAccount(prev => !prev)
     }
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
