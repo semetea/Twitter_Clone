a4352ab16610208bfe70645005b74e5b702fe048
semetea <55260856+semetea@users.noreply.github.com>
2022-08-29 15:20:46 -0400

    log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react"
-import { HashRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom"
+import Profile from "routes/Profile";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ?
                     <>
                         <Route exact path="/">
                             <Home />
                         </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
                     </> :
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>}
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route>
+                        <Redirect from="*" to="/" />
+                    </>
+                }
 
             </Switch>
         </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,13 @@
+import { authService } from "fbase"
 import React from "react"
+import { useHistory } from "react-router-dom"
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+    const history = useHistory()
+    const onLogoutClick = () => {
+        authService.signOut();
+        history.push("/")
+    }
+    return <><button onClick={onLogoutClick}>Log out</button></>
+}
 export default Profile;
